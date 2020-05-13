afabdfcc0466837bec09ccf1acc9ee8011e90db4
ashaaut <ashaautade1998@gmail.com>
2020-05-13 15:43:16 +0530

    added CSS property

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
 
 import React, { useState } from "react";
 import { hot } from 'react-hot-loader/root';
+import './assets/styles/App.css'
 
 function App() {
     const [name, setname] = useState("Asha");
     const updatename=(e)=>setname(e.target.value)
     return (
-        <div>
+        <div className="container">
             <input
                 type='text'
-                placeholder='enter the your name '
+                placeholder='Enter your name '
                 name='name'
                 onChange={updatename}>
             </input>

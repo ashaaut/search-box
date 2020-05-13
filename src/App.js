127bf2cb096eb78772409e0fa891cf8d9f9898ce
ashaaut <ashaautade1998@gmail.com>
2020-05-13 14:49:10 +0530

    added search box for updating name

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 
-import React from "react";
+import React, { useState } from "react";
 import { hot } from 'react-hot-loader/root';
 
-class App extends React.Component {
-  render() {
-    return <h1>Hello World</h1>;
-  }
+function App() {
+    const [name, setname] = useState("Asha");
+    const updatename=(e)=>setname(e.target.value)
+    return (
+        <div>
+            <input
+                type='text'
+                placeholder='enter the your name '
+                name='name'
+                onChange={updatename}>
+            </input>
+            <h1>Hello {name}</h1>
+        </div>
+    )
 }
 
 export default hot(App);
